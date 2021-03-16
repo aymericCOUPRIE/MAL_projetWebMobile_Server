20522d876fe619445c7b24adf08c8662c805161b
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2021-03-16 16:33:58 +0100

    ajout fonctionnalité isLogged()

--- routes/Users.js
+++ routes/Users.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const users = express.Router();
-const cors = require("cors");
+//const cors = require("cors");
 const jwt = require("jsonwebtoken"); //pour autentification
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
-const db = require("../database/db.js");
-const sequelize = require("sequelize");
-users.use(cors());
+
 
 //Connexion
 users.post("/login", (req, res) => {
@@ -14,24 +12,44 @@ users.post("/login", (req, res) => {
         where: {
             user_email: req.sanitize(req.body.email),
         },
-    })
-        .then((user) => {
-            if (!user) {
-                res.json({ error: "Email incorrect" });
+    }).then((user) => {
+        if (!user) {
+            res.json({error: "Email incorrect"});
+        } else {
+            if (bcrypt.compareSync(req.sanitize(req.body.password), user.user_motDePasse)) {
+                let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+                    expiresIn: Math.floor(Date.now() / 1000) + 60 * 60,
+                });
+                res.json({token: token, user: user.user_email});
             } else {
-                if (bcrypt.compareSync(req.sanitize(req.body.password), user.user_motDePasse)) {
-                    let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
-                        expiresIn: Math.floor(Date.now() / 1000) + 60 * 60,
-                    });
-                    res.json({ token: token, user: user });
-                } else {
-                    res.json({ error: "Mot de passe incorrect" });
-                }
+                res.json({error: "Mot de passe incorrect"});
+            }
+        }
+    }).catch((err) => {
+        res.send("error: " + err);
+    });
+});
+
+
+const verifyJWT = (req, res, next) => {
+    const token = req.headers["x-access-token"]
+    if (!token) {
+        res.send("There is no token")
+    } else {
+        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+            if (err) {
+                res.send({auth: false, message: "Auth failed"})
+            } else {
+                req.user_email = decoded.user_email
+                next()
             }
         })
-        .catch((err) => {
-            res.send("error: " + err);
-        });
-});
+    }
+}
+
+users.get('/isUserAuth', verifyJWT, (req, res) => {
+    res.send({auth: true, message: "Auth succeeded"})
+})
+
 
 module.exports = users
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 var app = express();
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
 
 //déclaration du port
 var port = process.env.PORT || 3000;
@@ -13,8 +15,10 @@ var sanitizer = require("express-sanitizer");
 //déclaration de ce que l'app utilise comme module
 app.use(sanitizer());
 app.use(bodyParser.json());
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser())
+
+app.use(cors())
 
 //pour utiliser des variables d'environnement
 const dotenv = require("dotenv");
@@ -22,7 +26,6 @@ dotenv.config();
 
 //déclaration des routes
 var Users = require("./routes/Users");
-
 app.use("/server", Users);
 
 //lancement serveur
