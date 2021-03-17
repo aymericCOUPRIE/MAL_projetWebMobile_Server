fa78bfd45e73104dc18f8a272d51cc379fbb682c
marineteroitin <marine.teroitin@gmail.com>
2021-03-17 19:22:44 +0100

    change token expiration time + delete isLogin

--- routes/Users.js
+++ routes/Users.js
@@ -17,7 +17,7 @@ users.post("/login", (req, res) => {
         } else {
             if (bcrypt.compareSync(req.sanitize(req.body.password), user.user_motDePasse)) {
                 let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
-                    expiresIn: Math.floor(Date.now() / 1000) + 60 * 60,
+                    expiresIn: "1d",
                 });
                 res.json({token: token, user: user.user_email});
             } else {
@@ -28,27 +28,6 @@ users.post("/login", (req, res) => {
         res.send("error: " + err);
     });
 });
-
-
-const verifyJWT = (req, res, next) => {
-    const token = req.headers["x-access-token"]
-    if (!token) {
-        res.send("There is no token")
-    } else {
-        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-            if (err) {
-                res.send({auth: false, message: "Auth failed"})
-            } else {
-                req.user_email = decoded.user_email
-                next()
-            }
-        })
-    }
-}
-
-users.get('/isUserAuth', verifyJWT, (req, res) => {
-    res.send({auth: true, message: "Auth succeeded"})
-})
 
 
 module.exports = users
