fa5e3f93f63b664beaf459fbae8022d0606dcedd
LBiasibetti <biasibetti.lra@gmail.com>
2021-03-20 22:11:18 +0100

    creation festival

--- routes/Festivals.js
+++ routes/Festivals.js
@@ -5,6 +5,53 @@ const Festival = require("../models/Festival");
 const { Op } = require('sequelize')
 
 
+/**
+ * Method which create a new festival
+ */
+festivals.post('/add', (req, res) => {
+    Festival.findOne({
+        where: {
+            fes_date: req.sanitize(req.body.fes_date),
+        },
+    })
+        .then((festival) => {
+            if (!festival) {
+                const festivalData = {
+                    fes_date: req.sanitize(req.body.fes_date),
+                    fes_nbTables: req.sanitize(req.body.fes_nbTables)
+                }
+                Festival.create(festivalData)
+                    .then((fes) => {
+                        res.json({success: "Festival créé avec succès !"})
+                    })
+                    .catch((err) => {
+                        res.json("error: " + err);
+                    })
+            } else {
+                res.json({error: "Un festival existe déjà à cette date."});
+            }
+        })
+});
+
+
+/**
+ * Method which get all the festivals
+ */
+festivals.get("/affichage",(req, res) => {
+    Festival.findAll()
+        .then((festivals) => {
+            if (!festivals) {
+                res.json({error: "Il n'y a aucun festival"});
+            } else {
+                res.json({allFestivals: festivals});
+            }
+        })
+        .catch((err) => {
+            res.send("error: " + err);
+        });
+});
+
+
 //prochain festival
 festivals.get("/closest", (req, res) => {
     Festival.findAll({
@@ -25,8 +72,6 @@ festivals.get("/closest", (req, res) => {
         res.send("error: " + err);
     });
 });
-
-
 
 
 module.exports = festivals;
